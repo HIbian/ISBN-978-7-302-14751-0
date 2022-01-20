dd46836b59a9d33a2a230857eef7a170e3ff16fc
hibian-arch <416711267@qq.com>
2022-01-20 10:53:35 +0800

    3.2.1 十进制数转换

--- Chapter3 栈和队列/3.1-3.2栈/test3.1.c
+++ Chapter3 栈和队列/3.1-3.2栈/test3.1.c
@@ -4,7 +4,10 @@
 #include "MyStack.h"
 #include <stdio.h>
 
+void practice3_1(int num, int d);
+
 int main() {
+    //test
     MyStack *s = InitStack();
     for (int i = 0; i < 5; ++i) {
         Push(s, i * 3);
@@ -22,5 +25,21 @@ int main() {
     StackTraverse(*s);
     printf("%d\n", StackLength(*s));
     DestroyStack(s);
+    //test end
+
+    //practice 3.2.1 十进制数转换
+    practice3_1(127, 2);
 
 }
+
+void practice3_1(int num, int d) {
+    MyStack *S = InitStack();
+    while (num) {
+        Push(S, num % d);
+        num /= d;
+    }
+    while (!StackEmpty(*S)){
+        printf("%d ",Pop(S));
+    }
+}
+
