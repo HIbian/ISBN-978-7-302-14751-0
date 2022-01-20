7c9ed4a4d30991a697fba619d1172257cb33118b
hibian-arch <416711267@qq.com>
2022-01-20 11:16:56 +0800

    3.2.2 括号匹配

--- Chapter3 栈和队列/3.1-3.2栈/test3.1.c
+++ Chapter3 栈和队列/3.1-3.2栈/test3.1.c
@@ -4,7 +4,9 @@
 #include "MyStack.h"
 #include <stdio.h>
 
-void practice3_1(int num, int d);
+void practice3_2_1(int num, int d);
+
+void practice3_2_2(char *str);
 
 int main() {
     //test
@@ -28,18 +30,46 @@ int main() {
     //test end
 
     //practice 3.2.1 十进制数转换
-    practice3_1(127, 2);
+    practice3_2_1(127, 2);
+
+    //3.2.2 括号匹配检测
+    practice3_2_2("(de({ow}ow32)");
 
 }
 
-void practice3_1(int num, int d) {
+void practice3_2_1(int num, int d) {
     MyStack *S = InitStack();
     while (num) {
         Push(S, num % d);
         num /= d;
     }
-    while (!StackEmpty(*S)){
-        printf("%d ",Pop(S));
+    while (!StackEmpty(*S)) {
+        printf("%d ", Pop(S));
     }
+    putchar('\n');
 }
 
+void practice3_2_2(char *str) {
+    char *p = str;
+    MyStack *s = InitStack();
+    while (*p != '\0') {
+        if (*p == '(' || *p == '{' || *p == '[') {
+            Push(s, *p);
+        } else if (*p == ')' || *p == '}' || *p == ']') {
+            char p_want = ' ';
+            if (*p == ')') p_want = '(';
+            else if (*p == '}') p_want = '{';
+            else if (*p == ']') p_want = '[';
+            if (p_want != Pop(s)) {
+                printf("括号不匹配");
+                return;
+            }
+        }
+        p++;
+    }
+    if(StackEmpty(*s)){
+        printf("括号匹配");
+    } else{
+        printf("括号不匹配");
+    }
+}
