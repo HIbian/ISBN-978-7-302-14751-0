ff8d03889d3a5b91f53420b98504ed02975ab7cd
hibian-arch <416711267@qq.com>
2022-02-16 11:03:30 +0800

    3.4循环队列头文件

---
+++ Chapter3 栈和队列/3.4队列/MyQueue.h
@@ -0,0 +1,30 @@
+#define True 1
+#define False 0
+
+typedef struct{
+    int *elem;
+    int maxSize;
+    int front;
+    int rear;
+}MyQueue;
+
+MyQueue* InitQueue();
+
+void DestoryQueue(MyQueue *q);
+
+void ClearQueue(MyQueue *q);
+
+int QueueEmpty(MyQueue q);
+
+int QueueLength(MyQueue q);
+
+int GetHead(MyQueue q);
+
+void EnQueue(MyQueue *q);
+
+int DeQueue(MyQueue *q);
+
+void QueueTRaverse(MyQueue q,void (*visit)(int));
+
+void visit(int elem);
+
