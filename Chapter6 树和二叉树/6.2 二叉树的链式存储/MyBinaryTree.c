3ecc763b1a77031f5f8b02dc9bda41809008d53e
hibian-arch <416711267@qq.com>
2022-03-23 16:44:49 +0800

    调整树构造方法，销毁树方法，调整代码格式

--- Chapter6 树和二叉树/6.2 二叉树的链式存储/MyBinaryTree.c
+++ Chapter6 树和二叉树/6.2 二叉树的链式存储/MyBinaryTree.c
@@ -6,8 +6,8 @@
 #include "stdio.h"
 
 //构造空二叉树
-MyBiTree *InitBiTree(){
-    MyBiTree *T = (MyBiTree*) malloc(sizeof(MyBiTree));
+MyBiTree *InitBiTree() {
+    MyBiTree *T = (MyBiTree *) malloc(sizeof(MyBiTree));
     if (!T) exit(-1);
     T->left = NULL;
     T->right = NULL;
@@ -16,48 +16,71 @@ MyBiTree *InitBiTree(){
 }
 
 //销毁二叉树，需要递归地释放空间
-void DestroyBiTree(MyBiTree *T){
-
+void DestroyBiTree(MyBiTree *T) {
+    if(T->left!=NULL){
+        DestroyBiTree(T->left);
+    }
+    if(T->right!=NULL){
+        DestroyBiTree(T->right);
+    }
+    free(T);
+    T = NULL;
 }
 
 //按照完全二叉树的顺序存储规则构造一棵链式存储的二叉树,definition第一位存放长度
-void CreateBiTree(MyBiTree *T,int *definition,int p){
-    if(p > definition[0] || definition[p] == 0){
-        T = NULL;
-        return;
-    }
+void CreateBiTree(MyBiTree *T, int *definition, int p) {
     T->elem = definition[p];
-    if (2*p <=definition[0]){
+    if (2 * p <= definition[0] && definition[2 * p] != 0) {
         T->left = InitBiTree();
-        CreateBiTree(T->left,definition,2*p);
+        CreateBiTree(T->left, definition, 2 * p);
+    } else {
+        T->left = NULL;
     }
-    if (2*p+1<=definition[0]){
+    if (2 * p + 1 <= definition[0] && definition[2 * p + 1] != 0) {
         T->right = InitBiTree();
-        CreateBiTree(T->right,definition,2*p+1);
+        CreateBiTree(T->right, definition, 2 * p + 1);
+    } else {
+        T->right = NULL;
     }
 }
-void ClearBiTree(MyBiTree *T);
-int BitTreeEmpty(MyBiTree T);
-int BitTreeDepth(MyBiTree T);
-MyBiTree *Root(MyBiTree T);
-//?
-int Value(MyBiTree T,Node e);
-void Assign(MyBiTree T,Node *e,int value);
-MyBiTree *Parent(MyBiTree T,Node e);
-MyBiTree *LeftChild(MyBiTree T,Node e);
-MyBiTree *RightChild(MyBiTree T,Node e);
-MyBiTree *LeftSibling(MyBiTree T,Node e);
-MyBiTree *RightSibling(MyBiTree T,Node e);
-//?
-void InsertChild(MyBiTree T,Node *p,int LR,MyBiTree *c);
-void DeleteChild(MyBiTree T,Node *p,int LR);
 
-void PreOrderTraverse(MyBiTree* T){
-    if(!T) return;
-    printf("%d ",T->elem);
+void ClearBiTree(MyBiTree *T);
+
+int BitTreeEmpty(MyBiTree T);
+
+int BitTreeDepth(MyBiTree T);
+
+MyBiTree *Root(MyBiTree T);
+
+//?
+int Value(MyBiTree T, Node e);
+
+void Assign(MyBiTree T, Node *e, int value);
+
+MyBiTree *Parent(MyBiTree T, Node e);
+
+MyBiTree *LeftChild(MyBiTree T, Node e);
+
+MyBiTree *RightChild(MyBiTree T, Node e);
+
+MyBiTree *LeftSibling(MyBiTree T, Node e);
+
+MyBiTree *RightSibling(MyBiTree T, Node e);
+
+//?
+void InsertChild(MyBiTree T, Node *p, int LR, MyBiTree *c);
+
+void DeleteChild(MyBiTree T, Node *p, int LR);
+
+void PreOrderTraverse(MyBiTree *T) {
+    if (!T) return;
+    printf("%d ", T->elem);
     PreOrderTraverse(T->left);
     PreOrderTraverse(T->right);
 }
+
 void InOrderTraverse(MyBiTree *T);
+
 void PostOrderTraverse(MyBiTree *T);
+
 void LevelOrderTraverse(MyBiTree *T);
--- Chapter6 树和二叉树/6.2 二叉树的链式存储/test6.2.c
+++ Chapter6 树和二叉树/6.2 二叉树的链式存储/test6.2.c
@@ -5,10 +5,25 @@
 #include "MyBinaryTree.h"
 #include "stdio.h"
 
-int main(){
-    int def[] = {11,1,2,3,4,5,6,7,8,9,10,11};
+int main() {
+    int def[] = {11, 1, 2, 3, 4, 5, 0, 0, 0, 0, 6, 7};
     MyBiTree *T = InitBiTree();
-    CreateBiTree(T,def,1);
+    CreateBiTree(T, def, 1);
     PreOrderTraverse(T);
+
+    /*
+     * 使用工具查看是否内存泄露
+     * valgrind --tool=memcheck --leak-check=full ./6.2
+     * 输出：
+     * ==6736== HEAP SUMMARY:
+     * ==6736==     in use at exit: 0 bytes in 0 blocks
+     * ==6736==   total heap usage: 8 allocs, 8 frees, 1,192 bytes allocated
+     * ==6736==
+     * ==6736== All heap blocks were freed -- no leaks are possible
+     * ==6736==
+     * ==6736== For lists of detected and suppressed errors, rerun with: -s
+     * ==6736== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)*
+     */
+    DestroyBiTree(T);
 }
 
